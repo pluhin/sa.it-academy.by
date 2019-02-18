9c99927e24f5c1e55d3e33ab9e7206e74b0c3643
Albert <pr-vk@bk.ru>
2019-02-18 19:51:31 +0300

    first commit to feature/do_one branch

--- ayehiazaran/sa2_02.git.sh
+++ ayehiazaran/sa2_02.git.sh
@@ -2,3 +2,4 @@ This is my first commit to master branch
 echo This is my second commit to master branch
 echo This is my first commit to dev branch
 echo This is my second commit to dev branch
+echo This is my first commit to feature/do_one branch
