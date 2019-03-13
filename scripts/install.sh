d108de07b6433e88d8f6cce446664ef0dcb0836f
Ivan Evseichik <ivan.evseichik@gmail.com>
2019-03-13 23:31:29 +0300

    add install.sh for packaages v1.1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' tcpdump|grep "install ok installed")
-if [ "" == "$PKG_OK" ]; sudo apt-get --only-upgrade install tcpdump then
-  sudo apt-get --force-yes --yes install tcpdump
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")
+if [ "" == "$PKG_OK" ];  then
+  sudo apt-get --force-yes --yes install $pkg
 fi 
 echo "All installed and updated"
