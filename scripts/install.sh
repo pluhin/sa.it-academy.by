249560bb38e7adcf63692952abc2c8687f18a1e4
Ivan Evseichik <ivan.evseichik@gmail.com>
2019-03-13 23:43:34 +0300

    add install.sh for nmap packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' nmap|grep "install ok installed")
 if [ "" == "$PKG_OK" ];  then
-  sudo apt-get --force-yes --yes install $pkg
+  sudo apt-get --force-yes --yes install nmap
 fi 
+sudo apt-get --only-upgrade install nmap
 echo "All installed and updated"
