f25a66bc21fe53695e366fff20e348dddbcbfe7e
Ivan Evseichik <ivan.evseichik@gmail.com>
2019-03-14 12:38:22 +0300

    add uninstall.sh v1.1

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' nmap|grep "install ok installed")
 if [ "" == "$PKG_OK" ];  then
-sudo apt purge-y nmap
+sudo apt purge -y nmap
 fi 
 echo "Removed nmap tools from machine"
