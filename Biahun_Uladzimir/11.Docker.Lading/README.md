6bffe8a8d20ef4a5d6ac45442208738374216455
Biahun_Uladzimir <begun74@gmail.com>
2019-07-22 11:30:19 +0300

    11.Docker.Lading new version 1.1

--- Biahun_Uladzimir/11.Docker.Lading/README.md
+++ Biahun_Uladzimir/11.Docker.Lading/README.md
@@ -6,5 +6,9 @@
 * Test it
 * Send/receive API request
 
+### Run docker run -p 80:80 my_flask:v1.1
+### URLs
+#### /persons
+#### /person/{0,1}
 
 [This poject on hub.docker.com](https://cloud.docker.com/u/begun74/repository/docker/begun74/my_flask)
--- Biahun_Uladzimir/11.Docker.Lading/app/main.py
+++ Biahun_Uladzimir/11.Docker.Lading/app/main.py
@@ -1,10 +1,63 @@
-from flask import Flask
+from flask import Flask , jsonify
+
+persons =  [
+   {
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 35,
+    "children": [
+        {
+            "firstName": "Alice",
+            "age": 6
+        },
+        {
+            "firstName": "Bob",
+            "age": 8
+        }
+    ]
+   },
+   {
+    "firstName": "Micky",
+    "lastName": "Mouse",
+    "hobbies": ["running", "sky diving"],
+    "age": 25,
+    "children": [
+        {
+            "firstName": "Alice",
+            "age": 6
+        }
+    ]
+   }
+
+]
 
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def home():
     return "<h2>Hello!</h2> <p><font color='red'>This is  11.Docker.Lading! </font> </p>"
+
+@app.route("/get")
+def get():
+    return "<h2>Get request</h2>"
+
+@app.route("/person/<int:id>" , methods=['GET'])
+def getPerson(id):
+    person = {}
+    try:
+     person = persons[id]
+    except:
+     person = {}
+    return person
+
+@app.route("/persons")
+def getPersons():
+    return jsonify({'persons': persons})
+
+
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0',port=80, debug=True)
