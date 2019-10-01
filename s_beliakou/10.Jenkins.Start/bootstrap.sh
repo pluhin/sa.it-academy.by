066565a9d42618df843c9af6189c865667819fe9
spooky1010 <53903155+spooky1010@users.noreply.github.com>
2019-10-01 11:04:45 +0300

    M sa2 09 19 (#416)

---
+++ s_beliakou/10.Jenkins.Start/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+yum install java wget git -y --nogpgcheck -q
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins -y --nogpgcheck -q
+usermod -s /bin/bash jenkins
+#sudo firewall-cmd --permanent --service=jenkins --add-port=8080/tcp
+#sudo firewall-cmd --reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+echo "##### Waiting for secret"
+#while [ ! -f /var/lib/jenkins/secrets/initialAdminPassword ]
+#do
+#    sleep 2
+#done
+sudo runuser -l jenkins -c 'echo -e "\n\n\n" | ssh-keygen -t rsa'
+sudo runuser -l jenkins -c 'cat ~/.ssh/id_rsa'
+sudo runuser -l jenkins -c 'cat ~/.ssh/id_rsa.pub'
+
+echo "##### Secret here"
+cat /var/lib/jenkins/secrets/initialAdminPassword
