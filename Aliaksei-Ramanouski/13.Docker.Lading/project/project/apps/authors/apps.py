1f24b3640209fb5ae72916060d654293dc3c856a
Aliaksei Ramanouski <manlyx10101@gmail.com>
2020-04-09 21:25:26 +0300

    13.Docker.Lading

--- Aliaksei-Ramanouski/13.Docker.Lading/project/project/apps/authors/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthorsConfig(AppConfig):
-    name = 'project.apps.authors'
-    verbose_name = 'Авторы'
--- Aliaksei-Ramanouski/13.Docker.Lading/project/project/apps/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'homepage'
-
-urlpatterns = [
-    path('', views.index_page, name='index'),
-    path('articles/', views.articles, name='articles')
-]
