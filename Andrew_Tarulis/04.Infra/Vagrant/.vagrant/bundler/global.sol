aecff805ff6944c23d74483a9b36500e481d21ef
Andrew Tarulis <ramzai138@gmail.com>
2020-07-30 17:18:28 +0300

    04.Infra-Vagrant

---
+++ Andrew_Tarulis/04.Infra/Vagrant/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"6fe96fc3837ddf06064b7c1235ea22516bda5ed254380cde30a933b9627a3949","vagrant_version":"2.2.9"}
