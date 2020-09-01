0bbefb614ae4d8f74b26fa7baed2f381237f26b3
Mikalai Rysnik <m.rysnik.42@gmail.com>
2020-09-01 15:19:03 +0300

    13.Docker.Landing

--- Mikalai_Rysnik/13.Docker.Landing/app.py
+++ Mikalai_Rysnik/13.Docker.Landing/app.py
@@ -25,22 +25,3 @@ def serverTCP(port=6000, host='127.0.0.1'):
 
 
 serverTCP(port=9090, host='')
-
-
-
-# import socket
-
-# sock = socket.socket()
-# sock.bind(('', 9090))
-# sock.listen(1)
-# conn, addr = sock.accept()
-
-# print('connected: ', addr)
-
-# while True:
-#     data = conn.recv(1024)
-#     if not data:
-#         break
-#     conn.send(data.upper())
-
-# conn.close()
--- Mikalai_Rysnik/13.Docker.Landing/client.py
+++ Mikalai_Rysnik/13.Docker.Landing/client.py
@@ -2,10 +2,10 @@
 import socket
 
 sock = socket.socket()
-sock.connect(('localhost', 9090))
+sock.connect(('127.0.0.1', 9090))
 sock.send(b'hello, world!')
 
-data = sock.recv(1024)
+data = sock.recv(512)
 sock.close()
 
 print(data)
