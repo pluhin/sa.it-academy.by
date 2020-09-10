196f572a43c325a6908049856ab7278e00e63164
alekseylasickiy <aleksey.lasickiy@gmail.com>
2020-09-10 10:17:25 +0300

    M sa2 13 20 (#897)

---
+++ Aleksey_Lasickiy/13.Docker.Lading/port_listner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+
+PORT = 8034
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("", PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
