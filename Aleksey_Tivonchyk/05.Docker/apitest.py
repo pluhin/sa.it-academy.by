3a4a282535cf01ee9b650837a2321b8831b1abb2
Aleksey Tivonchik <aliaksei.tsivonchyk@activecloud.com>
2020-09-24 17:16:50 +0300

    05.Docker

---
+++ Aleksey_Tivonchyk/05.Docker/apitest.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8080 # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ('Socket created')
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+print ('Socket bind complete')
+#Start listening on socket
+s.listen(10)
+print ('Socket now listening')
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+s.close()
+
