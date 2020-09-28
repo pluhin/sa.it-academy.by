8b581187e108f1ea92105c1edba492d79efb27b0
AlexandrAndreenko <71324315+AlexandrAndreenko@users.noreply.github.com>
2020-09-28 13:59:13 +0300

    06.Docker-compose_terraform_task (#938)

---
+++ Alexandr_Andreenko/06.Docker-compose_terraform/main.tf
@@ -0,0 +1,119 @@
+provider "docker" {}
+## Images
+resource "docker_image" "sonarr" {
+    name = "linuxserver/sonarr:latest"
+}
+resource "docker_image" "radarr" {
+    name = "linuxserver/radarr:latest"
+}
+resource "docker_image" "nginx" {
+    name = "nginx"
+}
+
+## Volumes and Network
+resource "docker_volume" "nginx_data" {
+  name = "nginx_data"
+}
+
+resource "docker_volume" "sonarr_config" {
+  name = "sonarr_config"
+}
+
+resource "docker_volume" "sonarr_tv" {
+  name = "sonarr_tv"
+}
+
+resource "docker_volume" "sonarr_downloads" {
+  name = "sonarr_downloads"
+}
+
+resource "docker_volume" "radarr_config" {
+  name = "radarr_config"
+}
+
+resource "docker_volume" "radarr_tv" {
+  name = "radarr_tv"
+}
+
+resource "docker_volume" "radarr_downloads" {
+  name = "radarr_downloads"
+}
+
+resource "docker_network" "network" {
+  name = "network"
+}
+
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.latest
+  restart = "always"
+  volumes {
+    volume_name    = docker_volume.nginx_data.name
+    container_path = "/usr/share/nginx/html"
+    read_only      = false
+  }
+  ports {
+    internal = "80"
+    external = "8080"
+  }
+  networks_advanced {
+    name = docker_network.network.name
+  }
+depends_on = ["docker_container.sonarr", "docker_container.radarr"]
+}
+
+resource "docker_container" "sonarr" {
+  name    = "sonarr"
+  image   = "${docker_image.sonarr.latest}"
+  restart = "always"
+  volumes {
+    volume_name    = docker_volume.sonarr_config.name
+    container_path = "/config"
+    read_only      = false
+  }
+  volumes {
+    volume_name    = docker_volume.sonarr_tv.name
+    container_path = "/tv"
+    read_only      = false
+  }
+  volumes {
+    volume_name    = docker_volume.sonarr_downloads.name
+    container_path = "/downloads"
+    read_only      = false
+  }
+  ports {
+    internal = "8989"
+    external = "8989"
+  }
+  networks_advanced {
+    name = docker_network.network.name
+  }
+}
+
+resource "docker_container" "radarr" {
+  name    = "radarr"
+  image   = "${docker_image.radarr.latest}"
+  restart = "always"
+  volumes {
+    volume_name    = docker_volume.radarr_config.name
+    container_path = "/config"
+    read_only      = false
+  }
+  volumes {
+    volume_name    = docker_volume.radarr_tv.name
+    container_path = "/tv"
+    read_only      = false
+  }
+  volumes {
+    volume_name    = docker_volume.radarr_downloads.name
+    container_path = "/downloads"
+    read_only      = false
+  }
+  ports {
+    internal = "7878"
+    external = "7878"
+  }
+  networks_advanced {
+    name = docker_network.network.name
+  }
+}
