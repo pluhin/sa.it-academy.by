f4d5cbe85b87c266222ca609f3ce579d9f4360ae
jintolia <68077135+jintolia@users.noreply.github.com>
2020-10-01 22:49:16 +0300

    Create server.py

---
+++ Anatoly_Katsubo/13.Docker.Lading/server.py
@@ -0,0 +1,15 @@
+import socket
+
+PORT = 9090
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("", PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
