39de4e5ce43c0acf059f60eef94fd30f26d525a2
Siarhei Pishchyk <pluhin@gmail.com>
2020-12-17 20:09:08 +0100

    10.K8s

---
+++ demo/10.K8s/first.groovy
@@ -0,0 +1,34 @@
+pipeline {
+
+  agent {
+    kubernetes {
+      label 'sample-app'
+      idleMinutes 5
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: curltest
+  namespace: ci-cd
+spec:
+  containers:
+  - name: curltest
+    image: tutum/curl
+    imagePullPolicy: IfNotPresent
+    command:
+    - cat
+    tty: true
+  restartPolicy: Always
+"""
+}
+  }
+  stages {
+    stage('Test') {
+      steps {
+        container('curltest') {
+          sh 'curl --version'
+        }
+      }
+    }
+  }
+}
