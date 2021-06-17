586946326f7630ba3d43b96379de2d3c73ecb43c
Igor Nemkovich <to.nemkovich@gmail.com>
2021-06-17 12:41:41 +0300

    replaсed if_else statement by while loop

--- inemkovich/03.Git.Hosting/git_push_2all_repos.sh
+++ inemkovich/03.Git.Hosting/git_push_2all_repos.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # 03.Git.Hosting
@@ -5,32 +6,30 @@
 
 REPO="./"
 
-git remote add github git@github.com:Magguro/it-academy.by.git &> /dev/null 
-git remote add gitlab git@gitlab.com:Magguro/it-academy.by.git &> /dev/null
-git remote add bitbucket git@bitbucket.org:magguro/it-academy.by.git &> /dev/null
+git remote add github git@github.com:Magguro/it-academy.by.git 2> /dev/null 
+git remote add gitlab git@gitlab.com:Magguro/it-academy.by.git 2> /dev/null
+git remote add bitbucket git@bitbucket.org:magguro/it-academy.by.git 2> /dev/null
 
-read -p "Do you want to push changes in current directory? (y/n)  " answ
-
-if [[ "$answ" == "y" ]]
-then
-  if [ ! -d ${REPO}/.git ]
-  then
-    echo "${REPO} is not a valid git repo! Aborting..."
+while true; do
+  read -p "Do you want to push changes in current directory? (y/n)  " answ
+  case $answ in 
+    [Yy]* ) \
+    if [ ! -d ${REPO}/.git ]
+    then
+      echo "${REPO} is not a valid git repo! Aborting..."
     exit 0
-  else
-    echo "${REPO} is a valid git repo! Proceeding..."
-  fi
-  git add --all
-  echo "enter commit message :  "
-  read comm_msg
-  git commit -m "$comm_msg"
-  git push --all github -f
-  git push --all gitlab -f
-  git push --all bitbucket -f
-elif [[ $answ == 'n' ]]
-then
-  exit 0
-else
-  echo "wrong answer"
-fi
+    else
+      echo "${REPO} is a valid git repo! Proceeding..."
+    fi
+    git add --all
+    echo "enter commit message :  "
+    read comm_msg
+    git commit -m "$comm_msg"
+    git push --all github -f
+    git push --all gitlab -f
+    git push --all bitbucket -f; break;;
+    [Nn]* ) exit 0;;
+    * ) echo "Please answer Y or N.";;
+  esac
+done
 
