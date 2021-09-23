85ca308b0de48537230a5fa643b567d8ac3e831d
FIX <fixpetrovich@gmail.com>
2021-09-23 10:21:28 +0000

    Homework+script

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git remote add origin git@github.com:FIXPETROVICH/03.GIT.Hosting.git
 git push -u origin --all
-git remote rename origin old-origin
+
 git remote add origin git@gitlab.com:fixpetrovich/devops.git
 git push -u origin --all
+
 git remote add origin git@bitbucket.org:FIXPETROVICH/03.git.hosting.git
-git remote rename origin bb-origin
 git push -u origin master
