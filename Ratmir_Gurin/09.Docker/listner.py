7c9cef56e65c0040d6c0ce37d50796da0f008edb
ratmir <ratmir.gurin@gmail.com>
2021-10-26 21:49:22 +0300

    09.Docker

---
+++ Ratmir_Gurin/09.Docker/listner.py
@@ -0,0 +1,15 @@
+# Echo server program
+import socket
+
+HOST = '0.0.0.0'        # Localhost interfaces
+PORT = 65001              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
