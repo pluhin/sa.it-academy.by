ae91bfed800b777ce2b86d62129b3cc269314a4b
Denis Oreshkevich <orehden@mail.ru>
2022-02-09 16:35:38 +0300

    09.Docker

---
+++ Denis_Oreshkevich/09.Docker/docker_hw/test_server/py_server.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT = 30001
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
