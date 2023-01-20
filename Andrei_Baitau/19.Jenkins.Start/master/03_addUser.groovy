8db478d351bc7fceac27ebee8295b83caa955382
AndreiBaitau <baitovandrei@gmail.com>
2023-01-20 15:47:57 +0300

    Hometask v19.1

--- Andrei_Baitau/19.Jenkins.Start/master/03_addUser.groovy
+++ Andrei_Baitau/19.Jenkins.Start/master/03_addUser.groovy
@@ -4,11 +4,11 @@ import hudson.security.*
 def instance = Jenkins.getInstance()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("baitov", HIDDEN)
+hudsonRealm.createAccount("abaitov", "abaitov")
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = (GlobalMatrixAuthorizationStrategy) instance.getAuthorizationStrategy()
-strategy.add(Jenkins.READ, "baitov")
+strategy.add(Jenkins.READ, "abaitov")
 instance.setAuthorizationStrategy(strategy)
 
 instance.save()
