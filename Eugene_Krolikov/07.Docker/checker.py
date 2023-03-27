f840bba515c70112b5254d07433b87c7960eb008
Krolikov Eugene <krolikov@bankdabrabyt.by>
2023-03-27 17:24:04 +0300

    push 07.Docker report

---
+++ Eugene_Krolikov/07.Docker/checker.py
@@ -0,0 +1,30 @@
+#python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = ""
+serverPort = 8443
+
+print("Server started http://%s:%s" % (hostName, serverPort))
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Python server!</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
