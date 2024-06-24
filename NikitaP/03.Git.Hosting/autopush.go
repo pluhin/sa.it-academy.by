c5053107ec6c8391488fc886eba324aed5ce9992
Nikita <vedro.for.emails@gmail.com>
2024-06-24 21:15:23 +0300

    Automatic commit of uncommitted changes

--- NikitaP/03.Git.Hosting/autopush.go
+++ NikitaP/03.Git.Hosting/autopush.go
@@ -8,51 +8,62 @@ import (
 )
 
 func main() {
-    // Checking for uncommitted changes
-    if hasUncommittedChanges() {
-        // If there are uncommitted changes, commit and push them
-        commitAndPushChanges()
-    } else {
-        fmt.Println("No uncommitted changes")
-    }
+	// Check for uncommitted changes
+	if hasUncommittedChanges() {
+		// If there are uncommitted changes, commit and push them
+		commitAndPushChanges()
+	} else {
+		fmt.Println("No uncommitted changes")
+	}
 }
 
-// Checking for uncommitted changes
+// Check for uncommitted changes
 func hasUncommittedChanges() bool {
-    cmd := exec.Command("git", "status", "--porcelain")
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
-    if err != nil {
-        log.Fatalf("Command execution error: %v", err)
-    }
-    // If the output is not empty, then there are uncommitted changes
-    return out.Len() > 0
+	cmd := exec.Command("git", "status", "--porcelain")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command execution error: %v", err)
+	}
+	// If the output is not empty, then there are uncommitted changes
+	return out.Len() > 0
 }
 
 // Commit and push changes
 func commitAndPushChanges() {
-    // Command to add all changes
-    cmdAdd := exec.Command("git", "add", ".")
-    err := cmdAdd.Run()
-    if err != nil {
-        log.Fatalf("Error adding changes: %v", err)
-    }
+	// Command to add all changes
+	cmdAdd := exec.Command("git", "add", ".")
+	err := cmdAdd.Run()
+	if err != nil {
+		log.Printf("Error adding changes: %v", err)
+		// If there is an error adding changes, push immediately
+		pushChanges()
+		return
+	}
 
-    // Command to commit changes
-    commitMessage := "Automatic commit of uncommitted changes"
-    cmdCommit := exec.Command("git", "commit", "-m", commitMessage)
-    err = cmdCommit.Run()
-    if err != nil {
-        log.Fatalf("Error committing changes: %v", err)
-    }
+	// Command to commit changes
+	commitMessage := "Automatic commit of uncommitted changes"
+	cmdCommit := exec.Command("git", "commit", "-m", commitMessage)
+	err = cmdCommit.Run()
+	if err != nil {
+		log.Printf("Error committing changes: %v", err)
+		// If there is an error committing changes, push immediately
+		pushChanges()
+		return
+	}
 
-    // Command for pushing changes
-    cmdPush := exec.Command("git", "push")
-    err = cmdPush.Run()
-    if err != nil {
-        log.Fatalf("Error pushing changes: %v", err)
-    }
+	// Push changes
+	pushChanges()
+}
 
-    fmt.Println("Changes successfully committed and pushed")
+// Push changes
+func pushChanges() {
+	cmdPush := exec.Command("git", "push")
+	err := cmdPush.Run()
+	if err != nil {
+		log.Fatalf("Error pushing changes: %v", err)
+	}
+
+	fmt.Println("Changes successfully pushed")
 }
