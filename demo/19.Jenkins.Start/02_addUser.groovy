0694d00e7bc257b30ccc4d9abfcba0437e8f545c
Siarhei Pishchyk <pluhin@gmail.com>
2024-11-18 21:18:21 +0200

    19.Jenkins Start

---
+++ demo/19.Jenkins.Start/02_addUser.groovy
@@ -0,0 +1,16 @@
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = (GlobalMatrixAuthorizationStrategy) instance.getAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, "admin")
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
+
