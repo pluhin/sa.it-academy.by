700e9bdffcfd846dbd0b27b1d3c0f6ad0f32c6d2
Siarhei Pishchyk <pluhin@gmail.com>
2024-12-23 20:46:20 +0200

    03 and 04 Git

---
+++ demo/04.GitAction/init_runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Need to run under regular user with sudo, not root!!!
+RUN_VERSION='2.XXX.0'
+RUN_REPO='pluhin/md-sa2-XX-XX'
+RUN_TOKEN='XXXXX'
+sudo apt-get update && sudo apt upgrade -yqq && sudo apt -yqq install curl
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux.tar.gz -L https://github.com/actions/runner/releases/download/v${RUN_VERSION}/actions-runner-linux-x64-${RUN_VERSION}.tar.gz
+tar xzf ./actions-runner-linux.tar.gz
+./config.sh --name my_test_runner --labels linux,my_runner --runnergroup default --url https://github.com/${RUN_REPO} --token ${RUN_TOKEN}
+./run.sh &
