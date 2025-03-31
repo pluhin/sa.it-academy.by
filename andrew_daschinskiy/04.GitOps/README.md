21dc88167f1c773deed5ac361fe29bc06c54bfb0
Андрей Дащинский <a.daschinskiy@gmail.com>
2025-03-31 15:59:48 +0300

    Add files via upload
    04.GitOps Home work

---
+++ andrew_daschinskiy/04.GitOps/README.md
@@ -0,0 +1,3 @@
+##Github actions
+- add hello.py
+- add hello.yml
---
+++ andrew_daschinskiy/04.GitOps/test_calculator.py
@@ -0,0 +1,7 @@
+import pytest
+from calculator import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
