ce92efcd62863b8ad6cb13881d9a307597ec7b65
Anastasia Sitsko <anastasia.akimova.v@gmail.com>
2025-11-30 18:26:46 +0300

    08 Docker-compose hw

---
+++ Anastasia_Sitsko/08.Docker.Docker-compose/HW_2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Multi-stage Docker Go App!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
+}
